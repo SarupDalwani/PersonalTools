d77b7d7d78ca698dd0f795c345877fccd9766b75
sarup <dalwanisarup@gmail.com>
2023-03-13 00:14:22 +0530

    Adding pdf spliter which can split pdf with given range.
    Usage : (for single page) python pdf_manager.py split input.pdf output.pdf page_number
    
    (for range of pages)
    python pdf_manager.py split input.pdf output.pdf start_page end_page where both start and end page are inclusive

--- PdfManager/pdf_manager.py
+++ PdfManager/pdf_manager.py
@@ -1,10 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import sys
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfWriter,PdfReader
 
 def merge_pdf(args) :
 
@@ -23,9 +18,34 @@ def merge_pdf(args) :
     merger.close()
     print(f'PDF merged to {merged_pdf}')
 
+def split_pdf(args) :
 
+    if len(args) < 3 :
+        print(f'Invalid arguments, required atleast 3 provided {len(args)}')
+        return
 
-# Press the green button in the gutter to run the script.
+    input_file_name = args[0]
+    input_file = open(input_file_name, 'rb')
+
+    input_file_reader = PdfReader(input_file)
+
+    output_file_writer = PdfWriter()
+    output_file_name = args[1]
+
+    start = int(args[2]) - 1
+    if len(args) == 3 :
+        end = start + 1
+    else :
+        end = int(args[3])
+
+    for page in range(start, end) :
+        output_file_writer.add_page(input_file_reader.pages[page])
+
+    output_file_writer.write(output_file_name)
+    output_file_writer.close()
+
+    print(f'PDF splited to {output_file_name}')
+
 if __name__ == '__main__':
     args = sys.argv
     len_args = len(sys.argv)
@@ -33,9 +53,9 @@ if __name__ == '__main__':
         print('No command provided, refer to help by --help')
     elif args[1] == 'merge' :
         merge_pdf(sys.argv[2:])
+    elif args[1] =='split' :
+        split_pdf(sys.argv[2:])
     elif args[1] == "--help" :
         print("merge [input_pdfs_path ...] output_path")
     else :
         print('No command provided, refer to help by --help')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
